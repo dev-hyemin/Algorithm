c53b8beab11992f9a0c04164aab20e37a560afd4
Kwonsofia <45736435+Kwonsofia@users.noreply.github.com>
2020-03-09 01:13:11 +0900

    0309

---
+++ baek_algorithm/src/baek_algorithm/Beak_11654.java
@@ -0,0 +1,12 @@
+package baek_algorithm;
+import java.util.*;
+public class Beak_11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		int c2 = (int)c;
+		System.out.println(c2);
+	}
+
+}
