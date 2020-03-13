4bbd7ea936fa4245b1d510c4e5988c1c1a73ac36
Kwonsofia <45736435+Kwonsofia@users.noreply.github.com>
2020-03-13 23:55:10 +0900

    0312

---
+++ baek_algorithm/src/baek_algorithm/Baek_10951.java
@@ -0,0 +1,15 @@
+package baek_algorithm;
+
+import java.util.Scanner;
+
+public class Baek_10951 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+}
