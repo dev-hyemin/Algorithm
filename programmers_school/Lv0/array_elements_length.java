76861146c7d4f5693b9b06ad9c5ba33996eba9b0
Kwonsofia <hyemin0723@swu.ac.kr>
2022-10-20 10:09:09 +0900

    2 day (v2)

--- programmers_school/Lv0/array_elements_length.java
+++ programmers_school/Lv0/array_elements_length.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] solution(String[] strlist) {
         int[] answer = new int[strlist.length];
-        for(int i=0;i<strlist.length;i++){
+        for(int i=0; i<strlist.length; i++){
             if (1<=strlist[i].length() && strlist[i].length()<=100){
                 answer[i] = strlist[i].length();
             }
